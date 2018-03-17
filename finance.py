f26f0aa20770afb9e4c886071e177de5fe700d05
Andre Ribes <andre.ribes@gmail.com>
2018-03-17 14:55:46 +1100

    Adding date argument to filter the results

--- finance.py
+++ finance.py
@@ -3,7 +3,10 @@
 # TODO - Clean raw of month / year before inserting again CSV data
 # TODO - Replace category table
 # TODO - Cash debit table
+# TODO - Parsing User
+# TODO - Review mois par category - mode review regex
 
+import sys
 import re
 import argparse
 from datetime import datetime
@@ -21,17 +24,21 @@ parser.add_argument("--add_csv_file", help="Add CSV file to the database")
 parser.add_argument("--add_csv_credit_file", help="Add CSV from credit mastercard file to the database")
 parser.add_argument("--test_regex", help="Test New Regex", action="store_true")
 parser.add_argument("--apply_regex", help="Apply New Regex", action="store_true")
+parser.add_argument("--apply_regexes", help="ReApply New Regex", action="store_true")
 parser.add_argument("--apply_custom", help="Apply Custom", action="store_true")
 parser.add_argument("--regex_category", help="New Regex Category")
 parser.add_argument("--regex_definition", help="New Regex Definition")
-args = parser.parse_args()
+parser.add_argument("--stats", help="Compute and Show statistics", action="store_true")
+parser.add_argument("--date", help="Select a specific date")
 
+args = parser.parse_args()
 
 conn = sqlite3.connect(db_filename)
 c = conn.cursor()
 
 # Checking if Table exists, if not create it
 c.execute('CREATE TABLE IF NOT EXISTS raw (date TEXT, amount REAL, description TEXT, acc_amount REAL, bank_category TEXT, username TEXT, category TEXT)')
+c.execute('CREATE TABLE IF NOT EXISTS custom (date TEXT, amount REAL, description TEXT, acc_amount REAL, bank_category TEXT, username TEXT, category TEXT)')
 c.execute('CREATE TABLE IF NOT EXISTS regex (regex TEXT, category TEXT)')
 conn.commit()
 
@@ -75,7 +82,43 @@ if args.apply_regex and args.regex_category and args.regex_definition:
   c.execute('INSERT INTO regex VALUES (?,?)', (args.regex_definition, args.regex_category))
   conn.commit()
 
+wanted_year  = None
+wanted_month = None
+wanted_day   = None
+if args.date:
+  try:
+    extracted_date = datetime.strptime(args.date, '%Y%m%d')
+  except ValueError:
+    try:
+      extracted_date = datetime.strptime(args.date, '%Y%m')
+    except ValueError:
+      try:
+        extracted_date = datetime.strptime(args.date, '%Y')
+      except ValueError:
+        print("Unknown date, please use: 2018, 201801 or 20180101")
+        sys.exit(1)
+        pass
+      else:
+        wanted_year  = extracted_date.strftime('%Y')
+    else:
+      wanted_year  = extracted_date.strftime('%Y')
+      wanted_month = extracted_date.strftime('%m')
+  else:
+    wanted_year  = extracted_date.strftime('%Y')
+    wanted_month = extracted_date.strftime('%m')
+    wanted_day   = extracted_date.strftime('%d')
+
+if wanted_year:
+  print("Wanted date is:", wanted_year, " ", wanted_month, " ", wanted_day)
+
+if args.apply_regexes:
+  utils.apply_regex_to_data(conn, c)
+  utils.apply_custom_recurrent(conn, c)
+
 if args.apply_custom:
   utils.apply_custom_recurrent(conn, c)
+
+if args.stats:
+  utils.compute_statistics(conn, c, wanted_year, wanted_month, wanted_day)
 
 conn.close()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+import pandas
 
 def insert_csv_date_in_db(conn, cursor, pd):
   for _, row in pd.iterrows():
@@ -39,3 +40,47 @@ def apply_custom_recurrent(conn, cursor):
   for row in row_to_change:
       cursor.execute('UPDATE raw SET category = ? WHERE date = ? AND amount = ? and description = ?', ('Orthophoniste', row[0], row[1], row[2]))
   conn.commit()
+
+def compute_statistics(conn, cursor, year, month, day):
+  all_raws = cursor.execute('SELECT * FROM raw')
+  df = pandas.DataFrame(all_raws.fetchall(), columns=['date', 'amount', 'description', 'acc_amount', 'bank_category', 'username', 'category'])
+  df = df[df.category != 'Exclude']
+
+
+  def keep_date(d, year=year, month=month, day=day):
+    d_year  = d[:4]
+    d_month = d[5:7]
+    d_day   = d[8:]
+    if year and d_year != year:
+      return False
+    if month and d_month != month:
+      return False
+    if day and d_day != day:
+      return False
+    return True
+
+  df['keep'] = df.date.apply(keep_date)
+  df = df[df.keep == True]
+  # Detect year/months and print for each one
+  df['date_filter'] = df.date.apply(lambda x : x[:-3])
+
+  df_groups_date = df.groupby(['date_filter'])
+
+  for name, group in df_groups_date:
+
+    data_per_category = group.groupby(['category']).agg({'amount': sum})
+    credit  = group[group.amount > 0.0].amount.sum()
+    debit   = group[group.amount <= 0.0].amount.sum()
+    restant = credit + debit
+    debit_proj = -9400 + group[group.category != 'Loyer'][group.amount <= 0.0].amount.sum()
+    restant_projection = credit + debit_proj
+
+    print('----------------')
+    print('Date:', group.date.iloc[0][:-3])
+    print(data_per_category)
+    print()
+    print('Credit:',  credit)
+    print('Debit:',   debit)
+    print('Restant:', restant)
+    print('Restant Projection:', restant_projection)
+    print('----------------')
